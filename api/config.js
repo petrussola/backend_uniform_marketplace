73fefd8316e6e7156d6a457dbdc32eebf4380637
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-06-06 22:49:45 +0100

    Connect mongoDB

--- api/config.js
+++ api/config.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 const config = {
   port: process.env.PORT,
+  devDbUri: process.env.DEV_DB_URI,
 };
 
 module.exports = config;
--- index.js
+++ index.js
@@ -1,10 +1,32 @@
+/* eslint-disable no-undef */
 const server = require("./api/server");
 require("dotenv").config();
+const { MongoClient } = require("mongodb");
 
 const config = require("./api/config");
 
 const port = config.port || 5000;
 
-server.listen(port, () => {
-  console.log(`\n\n Listening on port ${port}\n\n`);
+const client = new MongoClient(config.devDbUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  poolSize: 50,
+  wtimeout: 2500,
 });
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    await client.db().command({ ping: 1 });
+    console.log("Connected successfully to server");
+    server.listen(port, () => {
+      console.log(`\n\n Listening on port ${port}\n\n`);
+    });
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
